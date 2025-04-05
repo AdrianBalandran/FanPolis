ea0106ae9420e36534301e9a60b7ae697cf4e5c0
AdrianBalandran <adrianbala76@gmail.com>
2025-04-05 16:45:33 -0600

    Componente para la Pagina Principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PaginaprinComponent } from './paginaprin/paginaprin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PaginaprinComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
