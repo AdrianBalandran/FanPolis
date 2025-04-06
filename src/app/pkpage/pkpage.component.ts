cab5e251892f5d4aaa4b282af74f42128d557d14
AdrianBalandran <adrianbala76@gmail.com>
2025-04-06 10:05:13 -0600

    Implementación de Música a las paginas
    En la página de Rick and Morty y en Pokémon, implementé música, como también la lógica de que si cambia de página se deje de reproducir la música, como también un botón temático para silenciar o activar el audio.

--- src/app/pkpage/pkpage.component.ts
+++ src/app/pkpage/pkpage.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { NavegadorComponent } from "../navegador/navegador.component";
+import { Title } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pkpage',
   standalone: true,
-  imports: [],
+  imports: [NavegadorComponent, CommonModule],
   templateUrl: './pkpage.component.html',
   styleUrl: './pkpage.component.css'
 })
 export class PkpageComponent {
+
+  audio = new Audio();
+  isMuted = false;
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('FanPolis | Pokémon');
+    this.audio.src = '../../assets/audio/pktheme.mp3';
+    this.audio.loop = true;
+  }
+
+  ngOnInit() {
+    this.audio.play().catch(e => console.log('Error al reproducir audio:', e));
+  }
+
+  ngOnDestroy() {
+    this.audio.pause();
+  }
+
+  toggleMute() {
+    this.isMuted = !this.isMuted;
+    this.audio.muted = this.isMuted;
+  }
 
 }
--- src/app/rmpage/rmpage.component.ts
+++ src/app/rmpage/rmpage.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavegadorComponent } from "../navegador/navegador.component";
+import { Title } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-rmpage',
   standalone: true,
-  imports: [],
+  imports: [NavegadorComponent, CommonModule],
   templateUrl: './rmpage.component.html',
   styleUrl: './rmpage.component.css'
 })
-export class RmpageComponent {
+export class RmpageComponent implements OnInit, OnDestroy {
+  audio = new Audio();
+  isMuted = false;
 
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('FanPolis | Rick&Morty');
+    this.audio.src = '../../assets/audio/rmtheme.mp3';
+    this.audio.loop = true;
+  }
+
+  ngOnInit() {
+    this.audio.play().catch(e => console.log('Error al reproducir audio:', e));
+  }
+
+  ngOnDestroy() {
+    this.audio.pause();
+  }
+
+  toggleMute() {
+    this.isMuted = !this.isMuted;
+    this.audio.muted = this.isMuted;
+  }
 }
